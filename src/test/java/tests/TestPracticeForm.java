415eb8f3dac692a403e11ce6d9e7b6c77e88e2ed
fihch4 <fihch4@gmail.com>
2020-11-14 00:22:01 +0300

    Добавлена проверка введенных данных кроме даты календаря.

--- src/test/java/tests/TestPracticeForm.java
+++ src/test/java/tests/TestPracticeForm.java
@@ -7,24 +7,32 @@ import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.cssClass;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.by;
-import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+
 
 
 public class TestPracticeForm {
     @Test
     void TestAutomatin() {
+
+//Присваиваем значения переменным
         String name = "Dmitriy",
                 lastname = "Petrov",
                 email = "dmpetrov@mail.ru",
-                mobile = "99999999",
+                mobile = "9999999999",
                 subject = "English",
+                gender = "Male",
+                hobbies = "Sports",
                 current_address = "Lenina";
 
-
+//Открываем ссылку
         open("https://demoqa.com/automation-practice-form");
 
+//Ввод данных
         $("#firstName").val(name);
         $("#lastName").val(lastname);
         $("#userEmail").val(email);
@@ -34,9 +42,19 @@ public class TestPracticeForm {
         $(byXpath("//*[text()='Sports']")).click();
         $(byXpath("//*[contains(text(), 'Male')]")).click();
         $("#currentAddress").val(current_address);
+        $("#submit").scrollTo();
         $("#submit").click();
 
-        System.out.printf("");
+
+//Проверка на заполненные данные
+        $(byXpath("//td[text()='Student Name']/following::td[1]")).shouldHave(text(String.format(name + " " + lastname)));
+        $(byXpath("//td[text()='Student Email']/following::td[1]")).shouldHave(text(String.format(email)));
+        $(byXpath("//td[text()='Gender']/following::td[1]")).shouldHave(text(String.format(gender)));
+        $(byXpath("//td[text()='Mobile']/following::td[1]")).shouldHave(text(String.format(mobile)));
+        $(byXpath("//td[text()='Hobbies']/following::td[1]")).shouldHave(text(String.format(hobbies)));
+        $(byXpath("//td[text()='Address']/following::td[1]")).shouldHave(text(String.format(current_address)));
+
+        System.out.printf(String.format(name + " " + lastname));
 
     }
 
